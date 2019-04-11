c37073a82edcf2a1c70812300e49977764d46485
avnd26 <antoine@mlav.land>
2019-04-11 13:51:58 +0200

    finished relations for structures

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,15 +1,19 @@
 class Structure < ApplicationRecord
-	belongs_to :structure_type
-	belongs_to :structure_classification
+	#belongs_to :structure_type
+	#belongs_to :structure_classification
 
 	has_many :structure_individuals
 	has_many :individuals, through: :structure_individuals
 
-	has_many :parent_structures, foreign_key: "parent_id", class_name: "ParentChildRelation"
-	has_many :child_structures, foreign_key: "child_id", class_name: "ParentChildRelation"
+	has_many :is_parent, foreign_key: "parent_id", class_name: "ParentChildRelation"
+	has_many :is_child, foreign_key: "child_id", class_name: "ParentChildRelation"
+	has_many :child_structures, through: :is_parent, source: :child
+	has_many :parent_structures, through: :is_child, source: :parent
 
-	has_many :supported_structures, foreign_key: "supported_id", class_name: "StructureRelation"
-	has_many :supporting_structures, foreign_key: "supporting_id", class_name: "StructureRelation"
+	has_many :is_supported, foreign_key: "supported_id", class_name: "StructureRelation"
+	has_many :is_supported_by, through: :is_supported, source: :supporting
+	has_many :supports_structures, foreign_key: "supporting_id", class_name: "StructureRelation"
+	has_many :supports, through: :supports_structures, source: :supported
 
 	has_one :association_detail
 
