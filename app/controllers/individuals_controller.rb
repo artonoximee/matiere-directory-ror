85888ce72a17ba15a60de73896fe84db437fd82b
avnd26 <antoine@mlav.land>
2019-04-19 14:44:40 +0200

    added functionnalities to edit structures individuals relations

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,4 +1,6 @@
 class IndividualsController < ApplicationController
+  before_action :set_variables
+
   def index
     @individuals = Individual.all
   end
@@ -9,15 +11,13 @@ class IndividualsController < ApplicationController
   end
 
   def new
-    @structures = []
-    Structure.all.each do |structure|
-      @structures << [structure.name, structure.id]
-    end
   end
 
   def create
     individual = Individual.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], telephone: params[:telephone], linkedin: "http://" + params[:linkedin])
-    StructureIndividual.create(structure_id: params[:structure_id], individual: individual)
+    if params[:structure_id] != 0
+      StructureIndividual.create(structure_id: params[:structure_id], individual: individual)
+    end
     if params[:second_structure] == "1"
       StructureIndividual.create(structure_id: params[:structure_2_id], individual: individual)
     end
@@ -26,17 +26,55 @@ class IndividualsController < ApplicationController
 
   def edit
     @individual = Individual.find(params[:id])
+    @selected_structures = []
+    @individual.structures.each do |structure|
+      @selected_structures << [structure.name, structure.id]
+    end
   end
 
   def update
     @individual = Individual.find(params[:id])
     @individual.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], telephone: params[:telephone], linkedin: "http://" + params[:linkedin])
+    case
+      when @individual.structures != [] && params[:structure_id] == "0" && params[:structure_2_id] == "0"
+        @individual.structure_individuals.destroy_all
+
+      when @individual.structures.count == 1 && params[:structure_id] != "0" && params[:structure_2_id] != "0"
+        @individual.structure_individuals[0].update(structure_id: params[:structure_id], individual: @individual)
+        StructureIndividual.create(structure_id: params[:structure_2_id], individual: @individual)
+
+      when @individual.structures != [] && params[:structure_id] != "0" && params[:structure_2_id] != "0"
+        @individual.structure_individuals[0].update(structure_id: params[:structure_id], individual: @individual)
+        @individual.structure_individuals[1].update(structure_id: params[:structure_2_id], individual: @individual)
+
+      when @individual.structures != [] && params[:structure_id] == "0" && params[:structure_2_id] != "0"
+        @individual.structure_individuals[0].destroy
+        @individual.structure_individuals[1].update(structure_id: params[:structure_2_id], individual: @individual)
+
+      when @individual.structures != [] && params[:structure_id] != "0" && params[:structure_2_id] == "0"
+        @individual.structure_individuals[0].update(structure_id: params[:structure_id], individual: @individual)
+        @individual.structure_individuals[1].destroy
+
+      when @individual.structures == [] && params[:structure_id] != "0"
+        StructureIndividual.create(structure_id: params[:structure_id], individual: @individual)
+    end
     redirect_to individual_path(@individual.id)
   end
 
   def destroy
     individual = Individual.find(params[:id])
+    individual.structure_individuals.destroy_all
     individual.destroy
     redirect_to individuals_path
+  end
+
+  private
+
+  def set_variables
+    @structures = []
+    @structures << ["Pas de structure associée", 0]
+    Structure.all.each do |structure|
+      @structures << [structure.name, structure.id]
+    end
   end
 end
