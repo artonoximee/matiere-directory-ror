8fd112b05347194908c4e5c67482fd6b5e5d46d3
avnd26 <antoine@mlav.land>
2019-04-25 11:00:06 +0200

    added edit and destroy functions with corresponding views

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -16,11 +16,23 @@ class Admin::ProjectsController < ApplicationController
 	end
 
 	def edit
+		@project = Project.find(params[:id])
 	end
 
 	def update
+		project = Project.find(params[:id])
+		if params[:status] == "3"
+			project.update(title: params[:title], description: params[:description], website: "http://" + params[:website], year: params[:year], address: params[:address], zip_code: params[:zip_code], city: params[:city], country: params[:country], notes: params[:notes])
+		else
+			project.update(title: params[:title], description: params[:description], website: "http://" + params[:website], status: params[:status], year: params[:year], address: params[:address], zip_code: params[:zip_code], city: params[:city], country: params[:country], notes: params[:notes])
+		end
+		project.get_lat_lng
+		redirect_to project_path(project.id)
 	end
 
 	def destroy
+		project = Project.find(params[:id])
+		project.destroy
+		redirect_to projects_path
 	end
 end
